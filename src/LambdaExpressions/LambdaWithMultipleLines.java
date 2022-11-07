ec330093b94d7d0bd7978af9b705197fcdf03560
Osama Bin Azam <osamabinazam@gmail.com>
2022-11-08 01:05:27 +0500

    Working With Interfaces

--- src/LambdaExpressions/LambdaWithMultipleLines.java
+++ src/LambdaExpressions/LambdaWithMultipleLines.java
@@ -1,6 +1,14 @@
 
 
-public class LambdaWithMultipleLines{
+//Functional Interface
+public interface MyInterface {
+
+    //Abstract Method
+    public int printSum (int num1, int num2);
+
+    
+}
+public class LambdaWithMultipleLines implements MyInterface{
 
     public static void main (String [] args){
         new Thread (() -> {
@@ -10,6 +18,14 @@ public class LambdaWithMultipleLines{
             LambdaWithMultipleLines.printBio();
             
         }).start();
+
+        MyInterface ref;
+        int n1 = 3;
+        int n2 = 4;
+        System.out.println("Sum is : " + ref.);
+        
+
+
     }
     public static void printBio(){
         System.out.println("Name : Osama Bin Azam\nGender : Male\nAge : 21 years");
